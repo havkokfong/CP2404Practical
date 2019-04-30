3d3342249d9da4379805c0669ef49b943bfb903f
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-04-30 15:58:44 +0800

    prac 5 edited

--- Practical 5/src/InputCharacterInfo.java
+++ Practical 5/src/InputCharacterInfo.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class InputCharacterInfo {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your word: ");
-        String mChar = input.next();
+        System.out.println("Please enter a character: ");
+        String userinput = input.next();
+        char mChar = userinput.charAt(0);
+        if (Character.isUpperCase(mChar))
+            System.out.println(mChar + " is uppercase");
+        else
+            System.out.println(mChar + " is not uppercase");
+        if (Character.isLowerCase(mChar))
+            System.out.println(mChar + " is Lowercase");
+        else
+            System.out.println(mChar + " is not lowercase");
         }
     }
 
