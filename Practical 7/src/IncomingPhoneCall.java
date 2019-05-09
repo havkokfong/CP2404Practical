120414353ef3669508e62ceb2ba7d8c3f6529129
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-05-09 17:50:37 +0800

    prac 7 edited question 2a

---
+++ Practical 7/src/IncomingPhoneCall.java
@@ -0,0 +1,6 @@
+public class IncomingPhoneCall extends PhoneCall {
+    private IncomingPhoneCall(String phoneNum){
+        super(phoneNum);
+        price = 0.02;
+    }
+}
--- Practical 7/src/PhoneCall.java
+++ Practical 7/src/PhoneCall.java
@@ -2,6 +2,18 @@ public abstract class PhoneCall {
     String phoneNum;
     double price;
     public PhoneCall(String phoneNum){
+        price = 0.0;
+    }
 
+    private String getPhoneNum(){
+        return phoneNum;
+    }
+
+    private double getPrice(){
+        return price;
+    }
+
+    private void displayInfo(){
+        System.out.println(phoneNum + price);
     }
 }
